41f0191a1a8ffea552df8503e5856783b9a3d42b
yvan321 <neilyvancaliwan@gmail.com>
2025-07-17 18:59:42 +0800

    Added updated login

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,18 @@
 import Image from "next/image";
+import styles from "./styles/styles.module.css";
+import Header from "@/components/shared/header"; 
 
 export default function Home() {
   return (
     <div>
+      <Header /> 
+      <div
+          className={`${styles.parallaxSection} ${styles.parallax1}`}>
+          <p className={styles.text1}>UNIVERSITY  OF PERPETUAL  HELP SYSTEM DALTA</p>
+          <p className={styles.text2}>Digital Document Management</p>
+
+  
+        </div>
       <h1>Welcome to the Document Tracking System</h1>
       <p>This is a simple document tracking system built with Next.js.</p>
     </div>
---
+++ components/shared/header/index.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import Image from "next/image";
+import styles from "./header.module.css";
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.logoWrapper}>
+        <Image src="/full-logo.png" alt="Logo Icon" width={180} height={180} />
+        
+      </div>
+      <div className={styles.searchWrapper}>
+        <input
+          type="text"
+          placeholder="Search..."
+          className={styles.searchInput}
+        />
+      </div>
+
+          <div className={styles.authWrapper}>
+        <button className={styles.authButton}>
+          <a href="/sign-in">Log In</a>
+        </button>
+
+        <span className={styles.divider} />
+
+        <button className={styles.authButton}>
+          <a href="/sign-up">Sign Up</a>
+        </button>
+      </div>
+
+    </header>
+  );
+}
